e43396859c796cf6643fa1a3d816006ceb186612
WilPhil <117986238+WilPhil@users.noreply.github.com>
2025-05-09 12:05:20 +0700

    config: all html files

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+
 export default defineConfig({
   plugins: [tailwindcss()],
   base: "/upscale-clone",
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+        business: path.resolve(__dirname, "business.html"),
+        talent: path.resolve(__dirname, "talent.html"),
+        jobs: path.resolve(__dirname, "jobs.html"),
+        faq: path.resolve(__dirname, "faq.html"),
+      },
+    },
+  },
 });
